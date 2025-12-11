e4182f3d6ac0fb0813c21b73cc5d323606a0b8a3
Matej <matey.vesel@gmail.com>
2025-12-11 09:18:25 +0100

    feat: added simple spring back end with a /status controller.

---
+++ src/test/java/com/argocdcloudoperations/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argocdcloudoperations.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

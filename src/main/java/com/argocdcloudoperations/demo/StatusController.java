9a873790031b1855936f25fca0b1e7b9bcdfa127
Matej <matey.vesel@gmail.com>
2025-12-27 15:41:52 +0100

    feat: v0.1.9

--- src/main/java/com/argocdcloudoperations/demo/StatusController.java
+++ src/main/java/com/argocdcloudoperations/demo/StatusController.java
@@ -11,6 +11,6 @@ import java.util.Map;
 public class StatusController {
     @GetMapping
     public Map<String, String> getStatus() {
-        return Map.of("message", "Demo API development v0.1.9 (dev)");
+        return Map.of("message", "Demo API development v0.1.9 (qa)");
     }
 }
